882d6403e9093c43a83bb0e15f2cd4e4023f38eb
Hudhayfah <hoismail1430@gmail.com>
2022-09-28 14:02:27 -0700

    q0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
 
-
 def square_matrix_simple(matrix=[]):
-    new_array = list()
-    for i in range(0, len(matrix)):
-        new_array.append(list(map(lambda x: x**2, matrix[i])))
-    return 
+    """
+    Square each element of matrix
+    Args:
+        matrix - a 2d lists
+    """
+    if matrix is None:
+        return None
+    return [
+        [x**2 for x in row] for row in matrix
+    ]
+
+
+if __name__ == '__main__':
+    s = square_matrix_simple([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ])
+    print(s) 
